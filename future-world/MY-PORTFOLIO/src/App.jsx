bca1b821f0d71924eb346ff94ca05b0d930356ec
harishgithu <112798683+harishgithu@users.noreply.github.com>
2025-01-09 20:08:37 +0530

    portfolio-2025

--- future-world/MY-PORTFOLIO/src/App.jsx
+++ future-world/MY-PORTFOLIO/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Port from './Components/Port';
 import Contact from './Components/Contact';
-import Practice from './Practice';
+
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
         <Route path="/" element={<Port />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/port" element={<Port />} />
-        <Route path="/practice" element={<Practice />} />
+       
         {/* Add more routes here as needed */}
       </Routes>
     </BrowserRouter>
--- future-world/MY-PORTFOLIO/src/main.jsx
+++ future-world/MY-PORTFOLIO/src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import Practice from './Practice'
-import New from './Components/New'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <New />
+    <App />
   </React.StrictMode>,
 )
